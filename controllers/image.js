7270a441330fac0066d1bcd9a74a6d80a976d737
Marco Bihrle <bihrle.marco@hotmail.com>
2021-08-17 10:51:32 +0200

    first working structure

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require("Clarifai");
+
+const clarifaiApp = new Clarifai.App({
+    apiKey: "4060302e88684205ac4cf81edf5bad47",
+});
+
+const handleApiCall = (req, res) => {
+    clarifaiApp.models
+        .predict(Clarifai.GENERAL_MODEL, req.body.input)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => res.status(400).json("unable to work with API"));
+};
+
+const handleImage = (db) => (req, res) => {
+    const { id } = req.body;
+    db("users")
+        .where("id", "=", id)
+        .increment("entries", 1)
+        .returning("entries")
+        .then((entries) => {
+            res.json(entries[0]);
+        })
+        .catch((err) => res.status(400).json("unable to get entries"));
+};
+
+module.exports = {
+    handleImage,
+    handleApiCall,
+};
--- server.js
+++ server.js
@@ -2,118 +2,37 @@ const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const knex = require("knex");
 
-// const myPlaintextPassword = 's0/\/\P4$$w0rD';
-// const someOtherPlaintextPassword = 'not_bacon';
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+    client: "pg",
+    connection: {
+        host: "127.0.0.1",
+        user: "bihrl",
+        password: "bihrl",
+        database: "picture_analysis",
+    },
+});
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const database = {
-    users: [
-        {
-            id: "1",
-            name: "Marco",
-            email: "bihrle.marco@gmail.com",
-            password: "1",
-            entries: 0,
-            joined: new Date(),
-        },
-        {
-            id: "2",
-            name: "Maren",
-            email: "bihrle.maren@gmail.com",
-            password: "1234",
-            password: "2",
-            entries: 0,
-            joined: new Date(),
-        },
-    ],
-    // logins: [
-    //     {
-    //         id: "88",
-    //         hash: "",
-    //         email: "bihrle.marco@gmail.com",
-    //     },
-    // ],
-};
-
-app.get("/", (req, res) => {
-    res.json(database.users);
-});
-
-app.post("/signin", (req, res) => {
-    const { email, password } = req.body;
-    if (
-        email === database.users[0].email &&
-        password === database.users[0].password
-    ) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json("error logging in");
-    }
-});
-
-app.post("/register", (req, res) => {
-    const { email, name, password } = req.body;
-    // Hash password
-    // bcrypt.hash(password, saltRounds, function (err, hash) {
-    //     console.log("myHash: ", hash);
-    //     // Store hash in your password DB.
-    // });
-    database.users.push({
-        id: "3",
-        name,
-        email,
-        entries: 0,
-        joined: new Date(),
-    });
-    res.json(database.users[database.users.length - 1]);
-});
-
-app.get("/profile/:id", (req, res) => {
-    const { id } = req.params;
-    let userFound = false;
-    database.users.forEach((user) => {
-        if (user.id === id) {
-            userFound = true;
-            return res.json(user);
-        }
-    });
-    if (!userFound) {
-        res.status(404).json("no such user");
-    }
-});
-
-app.put("/image", (req, res) => {
-    const { id } = req.body;
-    let userFound = false;
-    database.users.forEach((user) => {
-        if (user.id === id) {
-            userFound = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    });
-    if (!userFound) {
-        res.status(404).json("no such user");
-    }
-});
-
-// // Hash password
-// bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
-//     // Store hash in your password DB.
+// app.get("/", (req, res) => {
+//     res.json(database.users);
 // });
 
-// // Load hash from your password DB.
-// bcrypt.compare(myPlaintextPassword, hash, function (err, result) {
-//     // result == true
-// });
-// bcrypt.compare(someOtherPlaintextPassword, hash, function (err, result) {
-//     // result == false
-// });
+app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/register", register.handleRegister(db, bcrypt, saltRounds));
+app.get("/profile/:id", profile.handleProfileGet(db));
+app.put("/image", image.handleImage(db));
+app.post("/imageurl", image.handleApiCall);
 
 app.listen(3000, () => {
     console.log("app is running on port 3000");
