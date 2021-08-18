e686ce6c81739350e44733b5116f1ea0928671d7
mbihrle <bihrle.marco@hotmail.com>
2021-08-18 13:59:12 +0200

    enable local db connection

--- server.js
+++ server.js
@@ -9,13 +9,23 @@ const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 
+// const db = knex({
+//     client: "pg",
+//     connection: {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: {
+//             rejectUnauthorized: false,
+//         },
+//     },
+// });
+
 const db = knex({
     client: "pg",
     connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false,
-        },
+        host: "127.0.0.1",
+        user: "bihrl",
+        password: "bihrl",
+        database: "picture_analysis",
     },
 });
 
